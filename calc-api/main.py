3fae8e1c91ae57e710b05faa067b31259bc9eb52
Ivan <ivanpugachivp@gmail.com>
2022-07-08 14:12:26 +0300

    Connected the database

--- calc-api/main.py
+++ calc-api/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
+import psycopg2
+from config import Settings
+import os
 
 app = FastAPI()
+settings = Settings()
+
+
+# conn = psycopg2.connect(dbname=settings.dbname, user=settings.user, host=settings.host,password=settings.password)
+# cur = conn.cursor()
+#
+# cur.execute("CREATE TABLE answer (id serial PRIMARY KEY, answer integer, operation varchar);")
+#
+# conn.commit()
+#
+# cur.close()
+# conn.close()
 
 
 class Item(BaseModel):
@@ -9,27 +24,46 @@ class Item(BaseModel):
     x: float
     y: float
 
-    # @validator('z')
-    # def onlySimpleMath(cls, v):
-    #     if v == '*' or v == '/' or v == '-' or v == '+':
-    #         return v
-    #     raise ValueError('only simple math requiert')
+    @validator('operation')
+    def onlySimpleMath(cls, v):
+        if v == '*' or v == '/' or v == '-' or v == '+':
+            return v
+        raise ValueError('only simple math requiert')
 
 
 @app.post("/")
 async def create_item(item: Item):
+    conn = psycopg2.connect(dbname=settings.dbname, user=settings.user, host=settings.host, password=settings.password)
+    cur = conn.cursor()
     if item.operation == "+":
-        return {"math_operation": item.operation, "answer": item.x + item.y}
-    if item.operation == "-":
-        return {"math_operation": item.operation, "answer": item.x - item.y}
+        answer = item.x + item.y
+        responce = {"math_operation": item.operation, "answer": answer}
 
-    if item.operation == "/":
+    elif item.operation == "-":
+        answer = item.x - item.y
+        responce = {"math_operation": item.operation, "answer": answer}
+
+    elif item.operation == "/":
         if item.y != 0:
-            return {"math_operation": item.operation, "answer": item.x / item.y}
+            answer = item.x / item.y
+            responce = {"math_operation": item.operation, "answer": answer}
         else:
-            return {"math_operation": "Division by zero!"}
+            responce = {"math_operation": "Division by zero!"}
 
-    if item.operation == "*":
-        return {"math_operation": item.operation, "answer": item.x * item.y}
+    elif item.operation == "*":
+        answer = item.x * item.y
+        responce = {"math_operation": item.operation, "answer": answer}
 
-    return {"math_operation": item.operation, "answer": "Invalid operation sign!"}
+    try:
+        with conn.cursor() as cursor:
+            cur.execute("INSERT INTO answer (answer, operation) VALUES (%s, %s)", (answer, item.operation))
+            conn.commit()
+
+    except (Exception, psycopg2.Error) as error:
+        conn.rollback()
+
+    cur.close()
+    conn.close()
+
+    return responce
+    # return {"math_operation": item.z, "answer": "Invalid operation sign!"}
